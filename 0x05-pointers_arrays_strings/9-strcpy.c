f3293cf333aa180de25244907ca66b2db36a8476
emz09 <mbreemems2@gmail.com>
2022-10-25 17:11:38 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,7 +2,7 @@
 
 
 /**
- * strcpy - a function that copies the string pointed to by src
+ * _strcpy - a function that copies the string pointed to by src
  *
  *  including the terminating null byte (\0)
  *
