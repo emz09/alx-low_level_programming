fb4804980636d985801d4487155b8bcf68dadbbe
emz09 <mbreemems2@gmail.com>
2022-11-02 15:33:49 -0700

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  * _strlen_recursion - find strlen of a string using recursion
+  * @s: string to find length of
+  * Return: number of bytes found
+  */
+
+
+int _strlen_recursion(char *s)
+
+{
+
+	if (*s == '\0')
+
+		return (0);
+
+	else
+
+		return (1 + _strlen_recursion(s + 1));
+
+}
