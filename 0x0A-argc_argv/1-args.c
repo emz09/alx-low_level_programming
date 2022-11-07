43aa2515ef730f2b3d9d130d372066e5503d3dbc
emz09 <mbreemems2@gmail.com>
2022-11-07 10:42:26 -0800

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+#include "main.h"
+
+
+/**
+  * main - print number of arguments
+  * @argc: number of arguments
+  * @argv: array containing arguments (made void)
+  * Return: 0 (success)
+  */
+
+int main(int argc, char *argv[])
+
+{
+
+	(void) **argv;
+
+
+	printf("%d\n", (argc - 1));
+
+
+	return (0);
+
+}
