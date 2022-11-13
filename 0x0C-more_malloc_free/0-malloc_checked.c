cd2366ab3c39bd8c6b2fb3ea56486d2308382deb
emz09 <mbreemems2@gmail.com>
2022-11-13 04:56:17 -0800

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <limits.h>
+
+
+/**
+  * malloc_checked - allocate memory using malloc
+  * @b: number of bytes to be allocated
+  * Return: 98 on failure, pointer to memory on success
+  */
+
+
+void *malloc_checked(unsigned int b)
+
+{
+
+	void *mem;
+
+	mem = malloc(b);
+
+	if (mem == NULL)
+
+		exit(98);
+
+	else
+
+		return (mem);
+}
